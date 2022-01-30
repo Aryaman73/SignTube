92e8c7b6b3b2d28fdf060c4f7544da4ce5d74317
Aryaman Singh <aryaman.singh.2012@gmail.com>
2022-01-30 00:58:30 -0500

    Transcription Backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
+import { useState } from 'react';
 import './App.css';
 import Button from '@mui/material/Button'
-
-import { useGradientBtnStyles } from '@mui-treasury/styles/button/gradient';
 
 
 function App() {
@@ -11,17 +10,43 @@ function App() {
   //   backgroundColor: 'rgba(255, 178, 79, 1)',
   // })
 
-  const styles = useGradientBtnStyles();
+  const [fileName, setFileName] = useState("sampleAudio.mp3")
+  const [text, setText] = useState("Nothing transcribed yet...");
+
+  function handleChange(event) {
+    console.log(`Selected file - ${event.target.files[0].name}`);
+    setFileName(event.target.files[0].name);
+  }
 
   return (
     <div className="App">
       <div className="recordButton">
-        <Button classes={styles} onClick={() => {
+        <Button onClick={() => {
           fetch("/run2")
         }}>
           Record
         </Button>
       </div>
+
+      <input type="file" accept="audio/*" onChange={handleChange} />
+
+      <div className="uploadButton">
+        <Button onClick={() => {
+          console.log("AAHHH", JSON.stringify({filename: fileName}));
+          fetch("/upload")
+            .then(res => res.json())
+            .then(data =>
+              {
+                console.log("HEY: ", data)
+                setText(data["text"])
+              })
+        }}>
+          TRANSCRIBE FILE
+        </Button>
+      </div>
+      <p>
+          {text}
+      </p>
     </div>
   );
 }
